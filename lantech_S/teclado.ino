8d76fce5141fe76bb592d9343b7afa09492089a1
= <=>
2023-02-03 07:31:04 -0300

    encontre qeu faltaba hacer un commit despues de mucho tiempo, habria que cheaquear

--- lantech_S/teclado.ino
+++ lantech_S/teclado.ino
@@ -1,9 +1,20 @@
-
-void keypadEvent(KeypadEvent customKey){
-  if (customKeypad.getState()== HOLD) {
-    Tx_Msg(customKey);
-    printDebug("HOLDEVENT:",String(customKey));
-  }
+#include <Servo.h>
+Servo servo1;
+ 
+int pin = 7;
+int pulsoMinimo = 580;
+int pulsoMaximo = 2500;
+int angulo = 0;
+int lectura;
+ 
+void setup() {
+  servo1.attach(pin,pulsoMinimo,pulsoMaximo);
+  pinMode(A0,INPUT);
 }
-
-
+ 
+void loop() {
+  lectura = analogRead(A7);
+  angulo = map(lectura,0,1023,0,180); // Escalar la lectura a un valor de 0 a 180
+  servo1.write(angulo);
+  delay(10);
+}
